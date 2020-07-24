9b34917e662e754bdba69a9e2f64fcc525df5f22
Melia Liu <liumy1@shanghaitech.edu.cn>
2020-07-24 17:13:00 +0800

    Added KeyboardEventListener

--- playing.py
+++ playing.py
@@ -5,24 +5,38 @@ from pylash.events import MouseEvent, LoopEvent, KeyboardEvent
 from pylash.media import Sound, MediaEvent
 from pylash.ui import LoadingSample1, Button
 
-score1 = 100
+score1 = 1
 score2 = 0
+
+gameBoard = []
+for i in range(50):
+    gameBoard.append([])
+    for j in range(50):
+        gameBoard[i].append(None)
 
 class snake(Sprite):
     def __init__(self, snakeKind, startX, startY, direction):
         super(snake, self).__init__()
 
         self.snakeKind = snakeKind
-        self.items = []
         self.direction = direction
         self.startX = startX
         self.startY = startY
         self.speed = 1
+        self.foodNumber = 0
+        self.head = {
+            "Up" : Bitmap(BitmapData(dataList[self.snakeKind+"HeadUp"])),
+            "Down" : Bitmap(BitmapData(dataList[self.snakeKind+"HeadUp"])),
+            "Left" : Bitmap(BitmapData(dataList[self.snakeKind+"HeadLeft"])),
+            "Right" : Bitmap(BitmapData(dataList[self.snakeKind+"HeadRight"]))
+        }
 
-        self.item = [Bitmap(BitmapData(dataList[self.snakeKind+"Head"+self.direction]))]
-        self.item[0].x = self.startX
-        self.item[0].y = self.startY
-        playingLayer.addChild(self.item[0])
+        self.items = [self.head[self.direction]]
+        self.items[0].x = self.startX
+        self.items[0].y = self.startY
+        playingLayer.addChild(self.items[0])
+        if (startX/10-1 >= 0) and (startY/10-13 >= 0):
+            gameBoard[int(startY/10-13)][int(startX/10-1)] = snakeKind
 
         if self.direction == "Up":
             for i in range(5):
@@ -37,35 +51,36 @@ class snake(Sprite):
             for i in range(5):
                 self.addBody(self.startY/10, self.startX/10-1-i)
 
-        # if (snakeKind == "snake1"):
-        #     head = Bitmap(BitmapData(dataList["snake1Head"+self.direction]))
-        #     head.x = startX + 14*10
-        #     head.y = startY + 44*10
-        #     playingLayer.addChild(head)
-        #     self.addBody(45, 14)
-        #     self.addBody(46, 14)
-        #     self.addBody(47, 14)
-        #     self.addBody(48, 14)
-        #     self.addBody(49, 14)
-        # else:
-        #     self.direction = "Down"
-        #     head = Bitmap(BitmapData(dataList["snake2Head"+self.direction]))
-        #     head.x = 10 + 35*10
-        #     head.y = 130 + 5*10
-        #     playingLayer.addChild(head)
-        #     self.addBody(4, 35)
-        #     self.addBody(3, 35)
-        #     self.addBody(2, 35)
-        #     self.addBody(1, 35)
-        #     self.addBody(0, 35)
-
     def addBody(self, row, col):
         item = Bitmap(BitmapData(dataList[self.snakeKind+"Body"]))
         item.x = col*10
         item.y = row*10
         playingLayer.addChild(item)
         self.items.append({"row":row, "col":col, "item":item})
+        row = int(row-13)
+        col = int(col-1)
+        if (row >= 0) and (col >= 0):
+            gameBoard[row][col] = self.snakeKind
+    
+    # def move(self):
+    #     # head style
+    #     self.items[0] = self.head[self.direction]
         
+    #     if self.direction == "Up":
+    #         pass
+    #     elif self.direction == "Down":
+    #         pass
+    #     elif self.direction == "Left":
+    #         pass
+    #     elif self.direction == "Right":
+    #         pass
+    
+    # def analyze(self):
+    #     pass
+
+    def loop(self):
+        print(self.snakeKind + " " + self.speed)
+
 
 
 def gameStart(data, bgmPlay, effectsPlay, p1Profile, p2Profile):
@@ -127,9 +142,65 @@ def gameStart(data, bgmPlay, effectsPlay, p1Profile, p2Profile):
     sampleSnake1 = snake("snake1", 130, 40, "Up")
     sampleSnake2 = snake("snake2", 380, 40, "Up")
 
+    global snake1, snake2
     # create game snakes
     snake1 = snake("snake1", 60, 180, "Right")
-    snake2 = snake("snake2", 450, 590, "Left")
+    snake2 = snake("snake2", 450, 570, "Left")
+
+    # stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown)
+    stage.addEventListener(KeyboardEvent.KEY_UP, keyUp)
+    playingLayer.addEventListener(LoopEvent.ENTER_FRAME, loop)
+
+# def keyDown(e):
+#     print("KeyDown")
+#     if e.keyCode == KeyCode.KEY_W:
+#         snake1.direction = "Up"
+#         snake1.speed = 2
+#         print("w")
+#     elif e.keyCode == KeyCode.KEY_S:
+#         snake1.direction = "Down"
+#         snake1.speed = 2
+#         print("s")
+#     elif e.keyCode == KeyCode.KEY_A:
+#         snake1.direction = "Left"
+#         snake1.speed = 2
+#         print("a")
+#     elif e.keyCode == KeyCode.KEY_D:
+#         snake1.direction = "Right"
+#         snake1.speed = 2
+#         print("d")
+#     elif e.keyCode == KeyCode.KEY_I:
+#         snake2.direction = "Up"
+#         snake2.speed = 2
+#         print("i")
+#     elif e.keyCode == KeyCode.KEY_K:
+#         snake2.direction = "Down"
+#         snake2.speed = 2
+#         print("k")
+#     elif e.keyCode == KeyCode.KEY_J:
+#         snake2.direction = "Left"
+#         snake2.speed = 2
+#         print("j")
+#     elif e.keyCode == KeyCode.KEY_L:
+#         snake2.direction = "Right"
+#         snake2.speed = 2
+#         print("l")
+
+def keyUp(e):
+    print("KeyUp")
+    if (e.keyCode == KeyCode.KEY_W) or (e.keyCode == KeyCode.KEY_S) or (e.keyCode == KeyCode.KEY_A) or (e.keyCode == KeyCode.KEY_D):
+        snake1.speed = 1
+    elif (e.keyCode == KeyCode.KEY_I) or (e.keyCode == KeyCode.KEY_K) or (e.keyCode == KeyCode.KEY_J) or (e.keyCode == KeyCode.KEY_L):
+        snake2.speed = 1
+
+    
+def loop(e):
+    if snake1 == 2:
+        print("snake1 == 2")
+    if snake2 == 2:
+        print("snake2 == 2")
+    # snake1.loop()
+    # snake2.loop()
 
 
 
